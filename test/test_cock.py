7a83f6005e81c0d72cc79511f817f1ded45616e2
MaxymHuang <huang.max-ym@gmail.com>
2024-09-19 10:41:38 +0800

    made some changes

--- test/test_cock.py
+++
@@ -1,7 +0,0 @@
-# test_random_forest.py
-
-def testcase():
-    print("my cock is huge")
-    return "my cock is huge"
-
-testcase()
---
+++ test/unittest.py
@@ -0,0 +1,8 @@
+# test_random_forest.py
+
+def testcase(number):
+    if number == 1: 
+        return "my cock is huge"
+if __name__ == "__main__":
+    print("running")
+    testcase(1)
